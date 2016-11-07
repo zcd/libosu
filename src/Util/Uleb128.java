f18ac5909dbf2897a268ff5a748c820850af4925
zcd <schiu.zcd@gmail.com>
2016-11-07 00:15:35 -0800

    Refactor to use a ULEB128 abstraction class. Add further test cases for overflow validation.

---
+++ src/Util/Uleb128.java
@@ -0,0 +1,75 @@
+package Util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+/**
+ * https://en.wikipedia.org/wiki/LEB128#Unsigned_LEB128
+ *
+ * Adopted from http://llvm.org/docs/doxygen/html/LEB128_8h_source.html
+ */
+public final class Uleb128 {
+    private final static int BITS_LONG = 64;
+    private final static int MASK_DATA = 0x7f;
+    private final static int MASK_CONTINUE = 0x80;
+    private final long value;
+
+    public static Uleb128 fromByteStream(InputStream bytes) throws IOException {
+        return new Uleb128(decode(bytes));
+    }
+
+    public static Uleb128 fromLong(long value) {
+        return new Uleb128(value);
+    }
+
+    public long asLong() {
+        return value;
+    }
+
+    public byte[] asBytes() {
+        return encode(value);
+    }
+
+    private Uleb128(long value) {
+        this.value = value;
+    }
+
+    private static long decode(InputStream bytes) throws IOException {
+        long value = 0;
+        int bitSize = 0;
+        int read;
+
+        do {
+            if ((read = bytes.read()) == -1) {
+                throw new IOException("Premature EOF");
+            }
+
+            value += ((long) read & MASK_DATA) << bitSize;
+            bitSize += 7;
+            if (bitSize >= BITS_LONG) {
+                throw new RuntimeException("ULEB128 value exceeds maximum value for long type.");
+            }
+
+        } while ((read & MASK_CONTINUE) == MASK_CONTINUE);
+        return value;
+    }
+
+    private static byte[] encode(long value) {
+        ArrayList<Byte> bytes = new ArrayList<Byte>();
+        do {
+            byte b = (byte) (value & MASK_DATA);
+            value >>= 7;
+            if (value != 0) {
+                b |= MASK_CONTINUE;
+            }
+            bytes.add(b);
+        } while(value != 0);
+
+        byte[] ret = new byte[bytes.size()];
+        for (int i = 0; i < bytes.size(); i++) {
+            ret[i] = bytes.get(i).byteValue();
+        }
+        return ret;
+    }
+}
---
+++ test/Util/Uleb128Test.java
@@ -0,0 +1,60 @@
+package Util;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import org.junit.Assert;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+/**
+ * Tests for Util.Uleb128
+ */
+public class Uleb128Test {
+    // TODO(zcd): fuzz testing?
+    @Test
+    public void readFrom() throws Exception {
+        Uleb128 parsed = Uleb128.fromByteStream(
+                new ByteArrayInputStream(new byte[] {(byte) 0xe5, (byte) 0x8e, (byte) 0x26}));
+        Assert.assertEquals(624485L, parsed.asLong());
+    }
+
+    @Test
+    public void readFrom_InvalidEmptyBuffer() throws Exception {
+        try {
+            Uleb128.fromByteStream(new ByteArrayInputStream(new byte[] {}));
+            Assert.fail();
+        } catch (IOException expected) {
+            Assert.assertThat(expected.getMessage(), CoreMatchers.containsString("Premature EOF"));
+        }
+    }
+
+    @Test
+    public void readFrom_ByteArrayOverflowsLongType() throws Exception {
+        byte[] yuuuge = new byte[] {
+                (byte) 0xff,
+                (byte) 0xff,
+                (byte) 0xff,
+                (byte) 0xff,
+                (byte) 0xff,
+                (byte) 0xff,
+                (byte) 0xff,
+                (byte) 0xff,
+                (byte) 0xff,
+                (byte) 0x01,
+        };
+        try {
+            Uleb128.fromByteStream(new ByteArrayInputStream(yuuuge));
+            Assert.fail();
+        } catch (RuntimeException expected) {
+            Assert.assertThat(expected.getMessage(), CoreMatchers.containsString("exceeds maximum value"));
+        }
+    }
+
+    @Test
+    public void asBytes() throws Exception {
+        Assert.assertArrayEquals(
+                new byte[] {(byte) 0xe5, (byte) 0x8e, (byte) 0x26}, Uleb128.fromLong(624485L).asBytes());
+    }
+}
