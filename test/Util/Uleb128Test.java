183e4b63c4e6df008b83df0898c61db4e7bc8941
zcd <schiu.zcd@gmail.com>
2016-11-09 01:59:54 -0800

    cleanup unused imports

--- test/Util/Uleb128Test.java
+++ test/Util/Uleb128Test.java
@@ -1,42 +1,22 @@
 package Util;
 
-import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
-import com.pholser.junit.quickcheck.generator.GenerationStatus;
-import com.pholser.junit.quickcheck.generator.Generator;
-import com.pholser.junit.quickcheck.generator.GeneratorConfiguration;
-import com.pholser.junit.quickcheck.generator.java.lang.ByteGenerator;
-import com.pholser.junit.quickcheck.generator.java.util.ArrayListGenerator;
-import com.pholser.junit.quickcheck.random.SourceOfRandomness;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Test;
-
-import org.junit.Assert;
 import org.junit.runner.RunWith;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.TYPE_USE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Tests for Util.Uleb128
  */
 @RunWith(JUnitQuickcheck.class)
 public class Uleb128Test {
-    // TODO(zcd): fuzz testing?
     @Test
     public void readFrom() throws Exception {
         Uleb128 parsed = Uleb128.fromByteStream(
