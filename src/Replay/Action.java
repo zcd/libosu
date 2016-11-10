f6d457799830ee33b86bf06a10da7d7aa91229f2
zcd <schiu.zcd@gmail.com>
2016-11-10 01:58:04 -0800

    apparently the cursor isn't always in the valid range? ok

--- src/Replay/Action.java
+++ src/Replay/Action.java
@@ -23,9 +23,6 @@ public final class Action {
      * @param keys combination of keys/mouse buttons pressed
      */
     public Action(long sincePrevActionMillis, float xPos, float yPos, EnumSet<KeyStroke> keys) {
-        Preconditions.checkArgument(0 <= xPos && xPos <= 512, "Cursor x-coordinate ∉[0,512]");
-        Preconditions.checkArgument(0 <= yPos && yPos <= 384, "Cursor y-coordinate ∉[0,512]");
-
         this.sincePrevActionMillis = sincePrevActionMillis;
         this.xPos = xPos;
         this.yPos = yPos;
