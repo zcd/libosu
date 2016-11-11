58ea096bd1c89e14a15004b6e49657a35f25a5ab
zcd <schiu.zcd@gmail.com>
2016-11-11 02:37:08 -0800

    use ordinals for gamemode enum

--- src/Constants/GameMode.java
+++ src/Constants/GameMode.java
@@ -4,14 +4,8 @@ package Constants;
  * See <a href="https://osu.ppy.sh/wiki/Osr_(file_format)#Data%20Types">osu! replay file format reference.</a>
  */
 public enum GameMode {
-    STANDARD((byte) 0x0),
-    TAIKO((byte) 0x1),
-    CATCH_THE_BEAT((byte) 0x2),
-    MANIA((byte) 0x3);
-
-    private final byte value;
-
-    GameMode(byte value) {
-        this.value = value;
-    }
+    STANDARD,
+    TAIKO,
+    CATCH_THE_BEAT,
+    MANIA
 }
